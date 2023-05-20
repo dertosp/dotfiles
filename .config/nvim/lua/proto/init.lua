c668ae24b6cac4df4b5250892356216b686abd79
dertosp <38324214+dertosp@users.noreply.github.com>
2023-05-20 17:26:00 +0300

    add plain config for neovim

---
+++ .config/nvim/lua/proto/init.lua
@@ -0,0 +1,31 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.clipboard:append { "unnamedplus" }
+
+-- vim.opt.tabstop = 4
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
+-- vim.opt.expandtab = true
+
+-- vim.opt.smartindent = true
+
+-- vim.opt.wrap = false
+
+-- vim.opt.swapfile = false
+-- vim.opt.backup = false
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- vim.opt.undofile = true
+
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
+vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<leader>b", vim.cmd.Ex)
+
+vim.keymap.set({"n", "v"}, "<C-d>", "<C-d>zz")
+vim.keymap.set({"n", "v"}, "<C-u>", "<C-u>zz")
+
