e9df0637b0742cf40ce313700997f459371f752c
dertosp <38324214+dertosp@users.noreply.github.com>
2023-05-21 21:31:35 +0300

    add .gitconfig

---
+++ install_mac.sh
@@ -0,0 +1,15 @@
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#  "formulae": [
+#    "jq",
+#    "fd",
+#    "fzf",
+#    "ripgrep",
+#    "tmux",
+#    "trex",
+#    "bitwarden"
+#  ],
+#  "casks": [
+#    "linearmouse",
+#    "alacritty",
+#    "bitwarden"
+#  ],
